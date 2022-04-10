fc497c4be74d9c4ad47732e8dcaac51c505b1191
Vina Afrilia <87349653+Vinaak@users.noreply.github.com>
2022-04-10 21:38:55 +0700

    Add files via upload
    calm

---
+++ js/questions.js
@@ -0,0 +1,101 @@
+const questions = [
+  {
+    "question": "Kurang berminat atau bergairah dalam melakukan apapun?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+  {
+    "question": "Merasa murung, sedih, atau putus asa?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+  {
+    "question": "Sulit tidur/mudah terbangun, atau terlalu banyak tidur?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+  {
+    "question": "Merasa lelah atau kurang bertenaga?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+  {
+    "question": "Kurang nafsu makan atau terlalu banyak makan?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+  {
+    "question": "Kurang percaya diri atau merasa bahwa Anda adalah orang yang gagal atau telah mengecewakan diri sendiri atau keluarga?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+  {
+    "question": "Sulit berkonsentrasi pada sesuatu, misalnya membaca koran atau menonton televisi?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+  {
+    "question": "Bergerak atau berbicara sangat lambat sehingga orang lain memperhatikannya. Atau sebaliknya; merasa resah atau gelisah sehingga Anda lebih sering bergerak dari biasanya?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+  {
+    "question": "Merasa lebih baik mati atau ingin melukai diri sendiri dengan cara apapun?",
+    "answer1": "Tidak Pernah",
+    "answer1Total": "0",
+    "answer2": "Beberapa hari",
+    "answer2Total": "1",
+    "answer3": "Lebih dari separuh waktu",
+    "answer3Total": "2",
+    "answer4": "Hampir setiap hari",
+    "answer4Total": "3"
+  },
+]
